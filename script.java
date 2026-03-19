736add7f942fda16fbd2d54d2c2b8af362b8b4e1
Douglas Guimaraes <douglasguimaraes234@gmail.com>
2026-03-18 21:03:22 -0300

    segundo commit

--- script.java
+++ script.java
@@ -1,5 +1,21 @@
+/*
 public class script{
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
 }
+ */
+public class script{
+    public static void main(String args[]) {
+        int idade = 10;
+        double salario = 1000;
+        boolean isMaiorIdade = idade >= 18;
+
+        if(isMaiorIdade = idade >= 18 ){
+            System.out.println("Maior de idade");
+        }
+        if(!isMaiorIdade){
+            System.out.println("Menor de idade");
+        }
+    }
+}    
