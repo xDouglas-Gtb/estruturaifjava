d3393172a9a10dde809c4059ad2e658883b62051
Douglas Guimaraes <douglasguimaraes234@gmail.com>
2026-03-29 19:54:17 -0300

    quarto commit

--- script.java
+++ script.java
@@ -55,6 +55,7 @@ public static void main(String[] args) {
 
  */
 
+/*
 public class script{
     public static void main(String[] args) {
             int idade = 18;
@@ -68,4 +69,32 @@ public class script{
             }
     }
 }
+
+*/
+
+
+
+public class script{
+    // utilizando switch e dados os valores de 1  a 7 , imprima se  é dia util ou final de semana 
+    // Considernando 1 como domingo
+    public static void main(String[] args) {
+        byte dia = 1 ;
+        switch(dia){
+            case 1 :
+            case 7 :
+                System.out.println("FDS");
+                break;
+            case 2:
+            case 3:
+            case 4:
+            case 5:
+            case 6:
+                System.out.println("Dia util");
+                break;
+            default:
+                System.out.println("Opçao Invalida");
+        }
+    }   
+}
+
 
